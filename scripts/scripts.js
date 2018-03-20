45cedb3ac866a8bcc43b85a605a19b6582a4adc8
sebamalisz <malisz654321@gmail.com>
2018-03-20 19:54:17 +0100

    repairing variable names

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -10,165 +10,165 @@
          let api = `https://api.openweathermap.org/data/2.5/weather?lat=${latLong[0]}&lon=${latLong[1]}&units=metric&APPID=1f08d3a7a4d09b2abb92db35a1c70794`
          $.getJSON(api, function (data) {
              console.log(data, data.name)
-             //info z tego programu o pogodzie:
-             // clouds - zachmurzenie podane w %;
-             let zachmurzenie = data.clouds.all;
-             // humidity - wilgotnosc podana w %;
-             let wilgotnosc = data.main.humidity;
-             // pressure - cisnienie w HpA
-             let cisnienie = data.main.pressure;
-             // temperatura
-             let temperatura = data.main.temp;
+             
+            
+             let clouds = data.clouds.all;
+             
+             let humidity = data.main.humidity;
+             
+             let pressure = data.main.pressure;
+            
+             let temperature = data.main.temp;
              // nazwa miasta
-             let miasto = data.name;
+             let city = data.name;
              // wschod slonca
-             let wschod = new Date(data.sys.sunrise * 1000);
-             let hours = wschod.getHours();
-             let minutes = "0" + wschod.getMinutes();
-             let wschodSlonca = hours + ':' + minutes.substr(-2);
+             let sunrise = new Date(data.sys.sunrise * 1000);
+             let hours = sunrise.getHours();
+             let minutes = "0" + sunrise.getMinutes();
+             let sunrise = hours + ':' + minutes.substr(-2);
              //zachod slonca
-             let zachod = new Date(data.sys.sunset * 1000);
-             let hoursz = zachod.getHours();
-             let minutesz = "0" + zachod.getMinutes();
-             let zachodSlonca = hoursz + ':' + minutesz.substr(-2);
+             let sunset = new Date(data.sys.sunset * 1000);
+             let hoursz = sunset.getHours();
+             let minutesz = "0" + sunset.getMinutes();
+             let sunset = hoursz + ':' + minutesz.substr(-2);
              //predkosc wiatru
-             let szybkoscWiatru = data.wind.speed;
+             let windSpeed = data.wind.speed;
              // kierunek wiatru
-             let kierunekWiatru = data.wind.deg;
+             let windDirection = data.wind.deg;
              //opis pogody (np deszcz)
-             let opisPogody = data.weather.description;
+             let weatherDesc = data.weather.description;
 
              // ustawianie opisu pogody 
              // const opisPogodyNazwa = ["",]
              // rysowanie w aplikacji
-             const miastoDiv = document.querySelector(".nazwa-miasta");
-             const temperaturaDiv = document.querySelector(".temperatura");
-             const zachmurzenieDiv = document.querySelector(".zachmurzenie");
-             const wilgotnoscDiv = document.querySelector(".wilgotnosc");
-             const cisnienieDiv = document.querySelector(".cisnienie");
-             const wiatrInfoDiv = document.querySelector(".wiatr-info");
-             const wiatrIconDiv = document.querySelector(".wiatr-icon");
-             const ikonaDiv = document.querySelector(".ikona");
-             const wschodDiv = document.querySelector(".wschod-godzina");
-             const zachodDiv = document.querySelector(".zachod-godzina");
+             const cityDiv = document.querySelector(".city-name");
+             const temperatureDiv = document.querySelector(".temperature");
+             const cloudsDiv = document.querySelector(".clouds");
+             const humidityDiv = document.querySelector(".humidity");
+             const pressureDiv = document.querySelector(".pressure");
+             const windInfoDiv = document.querySelector(".wind-info");
+             const windIconDiv = document.querySelector(".wind-icon");
+             const iconDiv = document.querySelector(".icon");
+             const sunriseDiv = document.querySelector(".sunrise");
+             const sunsetDiv = document.querySelector(".sunset");
 
              // ustawienie nazewnictwa zachmurzenia indeksy- 0: 0-20%(zachmurzenia), 1:21-40%, 2:41-60%, 3:61-80% 4:81-100%
-             var zachmurzenieNazwa = ["brak zachmurzenia", "niskie zachmurzenie", "średnie zachmurzenie", "wysokie zachmurzenie", "całkowite zachmurzenie"];
-             let terazZachmurzenie = "";
-             if ((zachmurzenie >= 0) && (zachmurzenie < 21)) {
-                 terazZachmurzenie = zachmurzenieNazwa[0]
+             var cloudsNames = ["brak zachmurzenia", "niskie zachmurzenie", "średnie zachmurzenie", "wysokie zachmurzenie", "całkowite zachmurzenie"];
+             let cloudsNow = "";
+             if ((clouds >= 0) && (clouds < 21)) {
+                cloudsNow = cloudsNames[0]
              }
-             if (zachmurzenie >= 21 && zachmurzenie < 41) {
-                 terazZachmurzenie = zachmurzenieNazwa[1];
+             if (clouds >= 21 && clouds < 41) {
+                cloudsNow = cloudsNames[1];
              };
-             if (zachmurzenie >= 41 && zachmurzenie < 61) {
-                 terazZachmurzenie = zachmurzenieNazwa[2];
+             if (clouds >= 41 && clouds < 61) {
+                cloudsNow = cloudsNames[2];
              };
-             if (zachmurzenie >= 61 && zachmurzenie < 81) {
-                 terazZachmurzenie = zachmurzenieNazwa[3];
+             if (clouds >= 61 && clouds < 81) {
+                cloudsNow = cloudsNames[3];
              };
-             if (zachmurzenie >= 81 && zachmurzenie < 100) {
-                 terazZachmurzenie = zachmurzenieNazwa[4];
+             if (clouds >= 81 && clouds < 100) {
+                cloudsNow = cloudsNames[4];
              };
-             // opis slowny cisnienia 
-             const cisnienieNazwa = ["niskie ciśnienie", "umiarkowane ciśnienie", "wysokie ciśnienie"];
-             let terazCisnienie = "";
-             if (cisnienie > 950 && cisnienie <= 990) {
-                 terazCisnienie = cisnienieNazwa[0]
+             
+             const pressureNames = ["niskie ciśnienie", "umiarkowane ciśnienie", "wysokie ciśnienie"];
+             let pressureNow = "";
+             if (pressure > 950 && pressure <= 990) {
+                pressureNow = pressureNames[0]
              }
-             if (cisnienie > 991 && cisnienie <= 1005) {
-                 terazCisnienie = cisnienieNazwa[1]
+             if (pressure > 991 && pressure <= 1005) {
+                pressureNow = pressureNames[1]
              }
-             if (cisnienie > 1006 && cisnienie <= 1050) {
-                 terazCisnienie = cisnienieNazwa[2]
+             if (pressure > 1006 && pressure <= 1050) {
+                pressure = pressureNames[2]
              }
              console.log("");
-             let tekstDoCzytania1 = 'jesteś w mieście' + data.name + " " + "temperatura wynosi " + temperatura + "stopni celsjusza." + "Występuje " + terazZachmurzenie + "Natomiast wiatr wieje z prędkością: " + szybkoscWiatru + " metrów na sekundę. Wilgotność powietrza wynosi " + wilgotnosc + "%.";
-             let tekstDoCzytania2 = 'Można dziś poczuć' + terazCisnienie + " o wartości " + cisnienie + " hektopaskali. Dzien rozpoczął się o godzinie " + wschodSlonca + " natomiast słońce zajdzie o godzinie " + zachodSlonca;
-             const msgPogoda1 = new SpeechSynthesisUtterance(tekstDoCzytania1);
-             const msgPogoda2 = new SpeechSynthesisUtterance(tekstDoCzytania2);
-             msgPogoda1.lang = 'pl-PL';
-             msgPogoda2.lang = 'pl-PL';
-             window.speechSynthesis.speak(msgPogoda1);
-             window.speechSynthesis.speak(msgPogoda2);
+             let textToRead1 = 'jesteś w mieście' + data.name + " " + "temperatura wynosi " + temperature + "stopni celsjusza." + "Występuje " + cloudsNow + "Natomiast wiatr wieje z prędkością: " + windSpeed + " metrów na sekundę. Wilgotność powietrza wynosi " + humidity + "%.";
+             let textToRead2 = 'Można dziś poczuć' + pressureNow + " o wartości " + pressure + " hektopaskali. Dzien rozpoczął się o godzinie " + sunrise + " natomiast słońce zajdzie o godzinie " + sunset
+             const msgWeather1 = new SpeechSynthesisUtterance(textToRead1);
+             const msgWeather2 = new SpeechSynthesisUtterance(textToRead2);
+             msgWeather1.lang = 'pl-PL';
+             msgWeather2.lang = 'pl-PL';
+             window.speechSynthesis.speak(msgWeather1);
+             window.speechSynthesis.speak(msgWeather2);
 
-             miastoDiv.innerHTML = "<span class='przed'>miasto: </span>" + miasto;
-             temperaturaDiv.innerHTML = "<span class='przed'>temperatura: </span>" + temperatura + "&deg";
-             zachmurzenieDiv.innerHTML = "<span class='przed'>zachmurzenie: </span>" + terazZachmurzenie;
-             wilgotnoscDiv.innerHTML = "<span class='przed'>wilg. powietrza: </span>" + wilgotnosc + " %";
-             cisnienieDiv.innerHTML = "<span class='przed'>ciśnienie: </span>" + cisnienie + " hPa";
-             wiatrInfoDiv.innerHTML = "<span class='przed'>wiatr:</span>" + szybkoscWiatru + "m/s";
-             wschodDiv.innerHTML = "<i class='em em-sunny'></i><span class='przed'> o godzinie  </span>" + wschodSlonca;
-             zachodDiv.innerHTML = "<i class='em em-sunrise'></i></i><span class='przed'> o godzinie  </span>" + zachodSlonca;
+             cityDiv.innerHTML = "<span class='przed'>miasto: </span>" + city;
+             temperatureDiv.innerHTML = "<span class='przed'>temperatura: </span>" + temperature + "&deg";
+             cloudsDiv.innerHTML = "<span class='przed'>zachmurzenie: </span>" + cloudsNow;
+             humidityDiv.innerHTML = "<span class='przed'>wilg. powietrza: </span>" + humidity + " %";
+             pressureDiv.innerHTML = "<span class='przed'>ciśnienie: </span>" + pressure + " hPa";
+             windInfoDiv.innerHTML = "<span class='przed'>wiatr:</span>" + windSpeed + "m/s";
+             sunriseDiv.innerHTML = "<i class='em em-sunny'></i><span class='przed'> o godzinie  </span>" + sunrise;
+             sunsetDiv.innerHTML = "<i class='em em-sunrise'></i></i><span class='przed'> o godzinie  </span>" + sunset;
              //  obrazy pogody
-             const obrazy = ["/pogoda/img/sunny.png", "/pogoda/img/cloudy.png", "/pogoda/img/partly-cloudy.png"];
-             if (terazZachmurzenie == "brak zachmurzenia") {
-                 Object.assign(ikonaDiv.style, {
-                     background: `url(${obrazy[0]})`,
+             const images = ["/pogoda/img/sunny.png", "/pogoda/img/cloudy.png", "/pogoda/img/partly-cloudy.png"];
+             if (cloudsNow == "brak zachmurzenia") {
+                 Object.assign(iconDiv.style, {
+                     background: `url(${images[0]})`,
                      backgroundSize: " contain",
                      backgroundRepeat: "no-repeat",
                      backgroundPosition: "center"
                      
                  });
              }
-             if (terazZachmurzenie == "niskie zachmurzenie") {
-                 Object.assign(ikonaDiv.style, {
-                     background: `url(${obrazy[2]})`,
+             if (cloudsNow == "niskie zachmurzenie") {
+                 Object.assign(iconDiv.style, {
+                     background: `url(${images[2]})`,
                      backgroundSize: " contain",
                      backgroundRepeat: "no-repeat",
                      backgroundPosition: "center"
                      
                  });
              }
-             if (terazZachmurzenie == "średnie zachmurzenie") {
-                 Object.assign(ikonaDiv.style, {
-                     background: `url(${obrazy[2]})`,
+             if (cloudsNow == "średnie zachmurzenie") {
+                 Object.assign(iconDiv.style, {
+                     background: `url(${images[2]})`,
                      backgroundSize: " contain",
                      backgroundRepeat: "no-repeat",
                      backgroundPosition: "center"
                      
                  });
              }
-             if (terazZachmurzenie == "wysokie zachmurzenie") {
-                 Object.assign(ikonaDiv.style, {
-                     background: `url(${obrazy[1]})`,
+             if (cloudsNow == "wysokie zachmurzenie") {
+                 Object.assign(iconDiv.style, {
+                     background: `url(${images[1]})`,
                      backgroundSize: " contain",
                      backgroundRepeat: "no-repeat",
                      backgroundPosition: "center"
                      
                  });
              }
-             if (terazZachmurzenie == "całkowite zachmurzenie") {
-                 Object.assign(ikonaDiv.style, {
-                     background: `url(${obrazy[1]})`,
+             if (cloudsNow == "całkowite zachmurzenie") {
+                 Object.assign(iconDiv.style, {
+                     background: `url(${images[1]})`,
                      backgroundSize: " contain",
                      backgroundRepeat: "no-repeat",
                      backgroundPosition: "center"
                      
                  });
              }
-             kierunekWiatru
-             Object.assign(wiatrIconDiv.style, {
+             windDirection
+             Object.assign(windIconDiv.style, {
                  background: `url("/pogoda/img/strzalka-wiatr.png")`,
                  backgroundSize: " 25% 80%",
                  backgroundRepeat: "no-repeat",
                  backgroundPosition: "center",
-                 transform: `translate(${kierunekWiatru}deg)`
+                 transform: `translate(${windDirection}deg)`
                 
              });
              // animacja pojawiania sie elementow
-             document.querySelector('.komenda-slowo').style.animationName = "schowaj";
-             document.querySelector('.komenda-info').style.animationName = "schowaj";
-             miastoDiv.style.animationName = "pojaw";
-             temperaturaDiv.style.animationName = "pojaw";
-             zachmurzenieDiv.style.animationName = "pojaw";
-             wilgotnoscDiv.style.animationName = "pojaw";
-             cisnienieDiv.style.animationName = "pojaw";
-             wiatrInfoDiv.style.animationName = "pojaw";
-             wiatrIconDiv.style.animationName = "pojaw";
-             ikonaDiv.style.animationName = "pojaw";
-             wschodDiv.style.animationName = "pojaw";
-             zachodDiv.style.animationName = "pojaw";
+             document.querySelector('.magic-word').style.animationName = "schowaj";
+             document.querySelector('.word-info').style.animationName = "schowaj";
+             cityDiv.style.animationName = "pojaw";
+             temperatureDiv.style.animationName = "pojaw";
+             cloudsDiv.style.animationName = "pojaw";
+             humidityDiv.style.animationName = "pojaw";
+             pressureDiv.style.animationName = "pojaw";
+             windInfoDiv.style.animationName = "pojaw";
+             windIconDiv.style.animationName = "pojaw";
+             iconDiv.style.animationName = "pojaw";
+             sunriseDiv.style.animationName = "pojaw";
+             sunsetDiv.style.animationName = "pojaw";
          });
 
 
