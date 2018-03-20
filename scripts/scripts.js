030f4bc33ff96078af4729af72b417ed961f34fc
sebamalisz <malisz654321@gmail.com>
2018-03-20 20:30:55 +0100

    update variables

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -144,10 +144,10 @@ function weatherNow() {
 }
 
 window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-const recognition = new SpeechRecognition();
+let recognition = new SpeechRecognition();
 recognition.interimResults = false;
 recognition.addEventListener('result', e => {
-    const transcript = Array.from(e.results)
+    let transcript = Array.from(e.results)
         .map(result => result[0])
         .map(result => result.transcript)
         .join('')
